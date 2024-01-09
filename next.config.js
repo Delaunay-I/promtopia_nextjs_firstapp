858b4017779f8a2d4027cf83b7874921fc6cf1f4
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2024-01-08 17:54:20 -0800

    finished authentication

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+      },
+}
 
 module.exports = nextConfig
+
+
+// const nextConfig = {
+//     experimental: {
+//       appDir: true,
+//       serverComponentsExternalPackages: ["mongoose"],
+//     },
+//     images: {
+//       domains: ['lh3.googleusercontent.com'],
+//     },
+//     webpack(config) {
+//       config.experiments = {
+//         ...config.experiments,
+//         topLevelAwait: true,
+//       }
+//       return config
+//     }
+//   }
